97d1de074dbfea1765713889a5513b489d4c2ed3
Vernon Louie <vernonlouie@aol.com>
2016-12-03 20:07:46 -0800

    calculator version1 converting to OOP -VL

--- main2.js
+++ main2.js
@@ -1,7 +1,10 @@
-/* Calculator project v0.5      Vernon Louie    C11.16      November 25, 2016 */
+/**
+ * Created by Vernon on 12/2/2016.
+ */
 
 var array1 = [" "];
 var index = 0;
+var obj_array = [];
 
 $(document).ready(function () {
     $(".number").click(number_clicked);         // Call function number_clicked when clicking on a number button
@@ -17,21 +20,27 @@ function special_clicked () {
     if (clearKey === "C") {
         // console.log("far");          // we are basically starting over, so reset values to a starting condition
         index = 0;
-        array1 = [" "];
+        obj_array = [" "];
         $(".display > h4").remove();    // destroys the h4 elements in the display
     } else {
         $("h4:last-child").text(" ");   // removes text from the last h4
-        array1[index] = " ";            // also need to clear out the contents of the array cell
+        obj_array.pop() = " ";            // also need to clear out the contents of the array cell
     }
 } // end of function special_clicked
 
 function equal_clicked () {
-    var num1 = Number(array1[0]);
-    var num2 = Number(array1[2]);
-    mathOper = array1[1];
+    var num1 = Number(obj_array[0].value);
+    var num2 = Number(obj_array[2].value);
+    mathOper = obj_array[1].value;
+
+    var just_clicked = new PunchTemplate ("equalSign", "=");
+    obj_array.push(just_clicked);
+    index++;
+
+    console.log("obj_array: " + obj_array);
 
     var result;
-    console.log("num1: " + num1 + "  num2: " + num2 + "  mathOper: " + mathOper);
+    console.log("mathOper: " + mathOper + "  obj_array: " + obj_array[0].value + " " + obj_array[1].value + " " + obj_array[2].value);
 
     if (mathOper === "/") {
         result = num1 / num2;
@@ -53,7 +62,8 @@ function operator_clicked () {
     var mathOperString = $(this).text();
     mathOperString = mathOperString.trim();
 
-    // console.log("mathOperString: " + mathOperString);
+    var just_clicked = new PunchTemplate ("operator", mathOperString);
+    obj_array.push(just_clicked);
     index++;
 
     var new_h4 = $("<h4>", {
@@ -61,33 +71,53 @@ function operator_clicked () {
     });
     $(".container1 .display").append(new_h4);
 
-    array1[index] = mathOperString;
+    console.log("a: " + obj_array[index].value + "  obj_array: " + obj_array + "  index: " + index);
     // console.log("index: " + index + " array1[" + index + "]: " + array1[index]);
-    index++;
-    array1[index] = " ";    // Add empty string to the last position of the array
+    // index++;
+    // Add empty string to the last position of the array
 } // end of function operator_clicked
 
 function number_clicked () {
     var numString = $(this).text();
     numString = numString.trim();   // .trim Removes white space from the string/text.
 
-    // console.log("numString: " + numString);
-    var isItEmpty = array1[index];
+    if (obj_array[index] === undefined) {
+        var just_clicked = new PunchTemplate ("number", numString);
+        obj_array.push(just_clicked);
 
-    if (isItEmpty === " ") {
-        array1[index] = numString;
         var new_h4 = $("<h4>", {
-            text: " " + array1[index] + " "
+            text: " " + obj_array[index].value + " "
         });
         $(".container1 .display").append(new_h4);
-    } else {
-        array1[index] = array1[index] + numString;
-        // console.log("index: " + index + array1[index]);
-        $(".container1 .display h4:last-child").text(array1[index]);
+
+        console.log("strange");
+    } else if (obj_array[index].type === "operator") {
+        var just_clicked = new PunchTemplate ("number", numString);
+        obj_array.push(just_clicked);
+        ++index;
+
+        var new_h4 = $("<h4>", {
+            text: " " + obj_array[index].value + " "
+        });
+        $(".container1 .display").append(new_h4);
+
+        console.log("rude");
     }
+    else {
+        obj_array[index].value = obj_array[index].value + numString;
+        $(".container1 .display h4:last-child").text(obj_array[index].value);
+    }
+
+    console.log("a: " + obj_array[index].value + "  obj_array: " + obj_array + "  index: " + index);
 } // end of function number_clicked
+
+function PunchTemplate (type, value) {
+    this.type = type;
+    this.value = value;
+}
 
 function gut (abc, def, ghi) {
     var array1 = [" "];
     $(".container1 .display").text(abc);
-}
+}var array1 = [" "];
+var index = 0;
