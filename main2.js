7842389650acb4e6dbaa6a6ae7d28d9a189f3fed
Vernon Louie <vernonlouie@aol.com>
2016-12-06 07:09:09 -0800

    slowly adding functionality to calculator -VL

--- main2.js
+++ main2.js
@@ -49,43 +49,85 @@ function number_clicked () {
 } // end of function number_clicked
 
 function operator_clicked () {
-    var mathOperString = $(this).text();
+    var mathOperString = $(this).text();    // grab the operator from the button text just clicked
     mathOperString = mathOperString.trim();
 
-    var just_clicked = new PunchTemplate ("operator", mathOperString);
-    obj_array.push(just_clicked);
-    index++;
+    var oper_string = obj_array[index].value;
+    var length = oper_string.length;
+    var array_of_operators = oper_string.split[""];     // convert string to array of strings with each string being 1 character long
 
-    var new_h3 = $("<h3>", {
-        text: mathOperString
-    });
-    $(".container1 .display").append(new_h3);
+    if (obj_array[index].type === "number") {
+        rid_decimals_if_any();  // in the number just entered before the operator sign, get rid of any extra decimals if any exist
+        var just_clicked = new PunchTemplate ("operator", mathOperString);  // give the new object a "type" of operator and a "value" of mathOperString
+        obj_array.push(just_clicked);
+        index++;    // increase index by 1 to stay matched with the just-pushed object
+
+        var new_h3 = $("<h3>", {
+            text: mathOperString
+        });
+        $(".container1 .display").append(new_h3);
+    } else if (mathOperString === array_of_operators[length - 1]) {
+        // append operator just clicked to what is already in the "value" property of the object, which should be a string of operators
+        obj_array[index].value = obj_array[index].value + mathOperString;
+        $(".container1 .display h3:last-child").text(obj_array[index].value);
+    }
 
     console.log("in operator_clicked, obj_array[index].value: " + obj_array[index].value + "  obj_array: " + obj_array + "  index: " + index);
 } // end of function operator_clicked
 
 function equal_clicked () {
     var result;
-    var num1 = Number(obj_array[0].value);
-    var num2 = Number(obj_array[2].value);
-    var mathOper = obj_array[1].value;
+    var num1;
+    var num2;
+    var mathOper;
+    var length = obj_array.length;  // note that I get the length of the array before I push the equal sign onto obj_array; important for the do loop below
+    var temp;
+
+    if (obj_array[index].type === "number") {
+        rid_decimals_if_any();  // in the number just entered before the equal sign, get rid of any extra decimals if any exist
+    }
 
     var just_clicked = new PunchTemplate ("equalSign", "=");
     obj_array.push(just_clicked);
     index++;
 
     console.log("in equal_clicked, obj_array: " + obj_array);
-    console.log("mathOper: " + mathOper + "  obj_array: " + obj_array[0].value + " " + obj_array[1].value + " " + obj_array[2].value);
+    console.log("obj_array: " + obj_array[0].value + " " + obj_array[1].value + " " + obj_array[2].value);
+
+    // always calculate result of first 2 numbers if there is more than 1 math operator and store it into temp
+    num1 = Number(obj_array[0].value);
+    num2 = Number(obj_array[2].value);
+    mathOper = obj_array[1].value;
 
     if (mathOper === "/") {
-        result = num1 / num2;
+        temp  = num1 / num2;
     } else if (mathOper === "x") {
-        result = num1 * num2;
+        temp = num1 * num2;
     } else if (mathOper === "-") {
-        result = num1 - num2;
+        temp = num1 - num2;
     } else {
-        result = num1 + num2;
+        temp = num1 + num2;
     }
+
+    // loop and do left most operators first; if there are only 2 numbers with 1 operator, then this for loop is not executed
+    for (var i=3; i < length; i+=2) {
+        num1 = temp;
+        mathOper = obj_array[i].value;
+        num2 = Number(obj_array[i+1].value);
+
+        // do 1 math operation at a time
+        if (mathOper === "/") {
+            temp  = num1 / num2;
+        } else if (mathOper === "x") {
+            temp = num1 * num2;
+        } else if (mathOper === "-") {
+            temp = num1 - num2;
+        } else {
+            temp = num1 + num2;
+        }
+        console.log("temp: " + temp);
+    }
+    result = temp;
 
     var new_h3 = $("<h3>", {
         text: " = " + result
@@ -117,8 +159,42 @@ function special_clicked () {
     }
 } // end of function special_clicked
 
+function rid_decimals_if_any () {   // check if there is more than 1 decimal point in the number at the current index before we push the operator or equal sign onto obj_array
+    var decimal_count = 0;
+    var num_string = (obj_array[index].value);
+    var string_length = num_string.length;      // 1st get length of string (number is really a string)
+    console.log("string_length: " + string_length);
+
+    var array_of_string = num_string.split("");     // convert string to array of strings with each string being 1 character long
+
+    // cycle through the string to check for and count decimal points
+    for (var i=0; i < string_length; ++i) {
+        if (array_of_string[i] === ".") {
+            decimal_count++;        // if there is a decimal point, increment by 1
+            console.log("decimal_count: " + decimal_count);
+        }
+    }
+
+    // if more than 1 decimal point, grab 2 substrings before and after the decimal points, but include the 1st decimal point only in temp_string1
+    if (decimal_count > 1) {
+        var first_decimal_position = num_string.indexOf(".");   // locate the 1st decimal point's position within the string
+        console.log("first: " + first_decimal_position);
+
+        // get 1st part that includes the decimal point
+        var temp_string1 = num_string.substr(0,first_decimal_position + 1);
+        // get the 2nd part that gets the string after the decimal points to the end of the string
+        var temp_string2 = num_string.substr(first_decimal_position + decimal_count, string_length);
+
+        console.log("temp_string1: " + temp_string1 + "  temp_string2: " + temp_string2);
+        num_string = temp_string1.concat(temp_string2); // concatenate the 2 parts into 1 string
+        console.log(num_string);
+        console.log("obj_array: " + obj_array);
+        obj_array[index].value = num_string;            // substitute the string minus the decimal points back into obj_array
+        console.log("obj_array: " + obj_array[index].value);
+    }
+}
+
 function PunchTemplate (type, value) {  // PunchTemplate as in punching a button
     this.type = type;       // type can be number, operator or equalSign
     this.value = value;     // value should be the value of the button just clicked
 }
-
